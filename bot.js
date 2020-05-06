52ef7a5f792bb6499544072038985f194b55414d
MasterLua <60834623+MasterLua@users.noreply.github.com>
2020-05-06 14:17:46 +0200

    Update DiscordBot
    i add ban , kick , clear commands i fix bug with prefix and fix bug for all perm

--- bot.js
+++ bot.js
@@ -37,11 +37,13 @@ client.on('message', message => {
   let args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   let command = args.shift().toLowerCase();
   let userid = message.author.id;
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
   if(command === "mod") {
     QueryDb("SELECT * FROM moderator WHERE DiscordID = '"+userid+"'", async function(result10){
       if(result10[0] && (result10[0].modo == 2) ){
       }else{
-        message.reply("You dont have permission")
+        message.channel.send("You dont have permission")
         return
       }
       let member = message.mentions.members.first();
@@ -56,25 +58,95 @@ client.on('message', message => {
         })
       }
       if(member == undefined) {
-        message.reply("Misuse of the command\nExample: !mod add/remove @blabla")
+        message.channel.send("Misuse of the command\nExample: !mod add/remove @blabla")
       }
       if(args[0] == undefined) {
-        message.reply("Misuse of the command\nExample: !mod add/remove @blabla")
+        message.channel.send("Misuse of the command\nExample: !mod add/remove @blabla")
       }
     })
     }
-    if(command == "admintest") {
+    if(command === "admintest") {
       QueryDb("SELECT * FROM moderator WHERE DiscordID = '"+userid+"'", async function(result99){
         if(result99[0] && (result99[0].modo == 1)){
-          message.reply("you're a moderator")
+          message.channel.send("you're a moderator")
           //your code if moderator
         }else if(result99[0] && (result99[0].modo == 2)){
-          message.reply("YOU HAVE FUCKING ALL PERM")
+          message.channel.send("YOU HAVE FUCKING ALL PERM")
           //your code if not moderator
         }else{
-          message.reply("You just user")
+          message.channel.send("You just user")
         }
       })
+    }
+    if(command === "ban") {
+      QueryDb("SELECT * FROM moderator WHERE DiscordID = '"+userid+"'", async function(result999){
+        if(result999[0] && (result999[0].modo == 1) || result999[0] && (result999[0].modo == 2)){
+          const user = message.mentions.users.first();
+          if(user === undefined) {
+            message.channel.send("[-] You need mentions user")
+            return
+          }
+          const member = message.guild.member(user);
+          let {guild} = message;
+          let raison = args.slice(1).join(' '); // arguments should already be defined
+          if(raison === undefined) {
+            message.channel.send("[-] You need say a reason")
+            return
+          }
+          member.send("[-] You have been banned from server : ``" + guild.name + "`` | Reason : ``" + raison + "``")
+          member
+          .ban({
+            reason: raison,
+          })
+          message.channel.send("[+] Successfully banned")
+        }else{
+          message.channel.send("[-] You dont have permission")
+        }
+        })
+    }
+    if(command === "kick") {
+      QueryDb("SELECT * FROM moderator WHERE DiscordID = '"+userid+"'", async function(result9999){
+        if(result9999[0] && (result9999[0].modo == 1) || result9999[0] && (result9999[0].modo == 2)){
+          const user = message.mentions.users.first();
+          if(user === undefined) {
+            message.channel.send("[-] You need mentions user")
+            return
+          }
+          const member = message.guild.member(user);
+          let {guild} = message;
+          let raison = args.slice(1).join(' '); // arguments should already be defined
+          if(raison === undefined) {
+            message.channel.send("[-] You need say a reason")
+            return
+          }
+          member.send("[-] You have been kicked from server : ``" + guild.name + "`` | Reason : ``" + raison + "``")
+          member
+          .kick({
+            reason: raison,
+          })
+          message.channel.send("[+] Successfully kicked")
+        }else{
+          message.channel.send("[-] You dont have permission")
+        }
+        })
+    }
+    if(command === "clear") {
+      QueryDb("SELECT * FROM moderator WHERE DiscordID = '"+userid+"'", async function(result9999){
+        if(result9999[0] && (result9999[0].modo == 1) || result9999[0] && (result9999[0].modo == 2)){
+    }else{
+      message.channel.send("[-] You dont have permission")
+      return
+    }
+})
+      const args = message.content.split(' ').slice(1); // All arguments behind the command name with the prefix
+      const amount = args.join(' '); // Amount of messages which should be deleted
+      if (!amount) return message.reply('You haven\'t given an amount of messages which should be deleted!'); // Checks if the `amount` parameter is given
+      if (isNaN(amount)) return message.reply('The amount parameter isn`t a number!'); // Checks if the `amount` parameter is a number. If not, the command throws an error
+      if (amount > 100) return message.reply('You can`t delete more than 100 messages at once!'); // Checks if the `amount` integer is bigger than 100
+      if (amount < 1) return message.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
+      message.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
+      message.channel.bulkDelete(messages)
+});
     }
 });
 
